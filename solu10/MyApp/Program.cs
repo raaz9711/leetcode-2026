4298ae7fde3b91f1223cede84d11efb3fbb9c640
raaz9711 <raaz.kumar015@gmail.com>
2026-06-15 21:59:30 +0530

    -- PriortyQueue

--- solu10/MyApp/Program.cs
+++ solu10/MyApp/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Solu10!");
+﻿using DSAImp;
+
+
+var q = new Helper();
+q.AddElement();
+
+List<Bug> list = new List<Bug>();
+
+while(q.pq.Count > 0)
+{
+    var v = q.pq.Dequeue();
+    Console.WriteLine(v.Priorty);
+    Console.WriteLine("---------------");
+    v.IsResolve = true;
+    v.Priorty *= 10;
+    list.Add(v);
+}
+
+foreach(var a in list)
+{
+    Console.WriteLine(a.ToString());
+}
